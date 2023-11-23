49c07885d998a06d0b1d85752260adaa529017e6
SydneyWamalwa <47521632+SydneyWamalwa@users.noreply.github.com>
2023-11-23 17:32:35 +0300

    fixed payments

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, session,jsonify
 import sqlite3
 import os
-import requests
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Change this to a secure secret key
